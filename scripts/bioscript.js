80b9589e07a329110525ef86ed5961a4c2034060
Vanesa <vaneebg@gmail.com>
2022-04-11 17:20:33 +0200

    por fin acabada function para los botones

--- scripts/bioscript.js
+++ scripts/bioscript.js
@@ -1,11 +1,80 @@
-// Agregar listener
-
-let botones = document.getElementById("scroll")
-
-function visibility() {
-    if (botones.style.display != "none") {
-        botones.style.display = "none"
+// boton 1 datapersonal
+function visibility1() {
+    let botones1 = document.getElementById("scroll1");
+    if (botones1.style.display != "none") {
+        botones1.style.display = "none"
     } else {
-        botones.style.display = "block"
+        botones1.style.display = "flex"
+    }
+}
+// boton 2 hobbies
+function visibility2() {
+    let botones2 = document.getElementById("scroll2");
+    if (botones2.style.display != "none") {
+        botones2.style.display = "none"
+    } else {
+        botones2.style.display = "flex"
+    }
+}
+// boton 3 dibujo
+function visibility3() {
+    let botones3 = document.getElementById("scroll3");
+    if (botones3.style.display != "none") {
+        botones3.style.display = "none"
+    } else {
+        botones3.style.display = "flex"
+    }
+}
+// boton 4 sport
+function visibility4() {
+    let botones4 = document.getElementById("scroll4");
+
+    if (botones4.style.display != "none") {
+        botones4.style.display = "none"
+    } else {
+        botones4.style.display = "flex"
+    }
+}
+// mboton 5 anime
+function visibility5() {
+    let botones5 = document.getElementById("scroll5");
+
+    if (botones5.style.display != "none") {
+        botones5.style.display = "none"
+    } else {
+        botones5.style.display = "flex"
+    }
+}
+
+// boton 6 gamer
+function visibility6() {
+    let botones6 = document.getElementById("scroll6");
+
+    if (botones6.style.display != "none") {
+        botones6.style.display = "none"
+    } else {
+        botones6.style.display = "flex"
+    }
+}
+
+// boton 7 origami
+function visibility7() {
+    let botones7 = document.getElementById("scroll7");
+
+    if (botones7.style.display != "none") {
+        botones7.style.display = "none"
+    } else {
+        botones7.style.display = "flex"
+    }
+}
+
+// boton 8 academicos
+function visibility8() {
+    let botones8 = document.getElementById("scroll8");
+
+    if (botones8.style.display != "none") {
+        botones8.style.display = "none"
+    } else {
+        botones8.style.display = "flex"
     }
 }
