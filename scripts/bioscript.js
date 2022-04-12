2b564e82e2393dc4ec84242afb8301b2f0a1567b
Vanesa <vaneebg@gmail.com>
2022-04-12 14:14:36 +0200

    arreglada function js y añadido contenido anime

--- scripts/bioscript.js
+++ scripts/bioscript.js
@@ -1,48 +1,50 @@
 // boton 1 datapersonal
+let botones1 = document.getElementById("scroll1") || [];
+
 function visibility1() {
-    let botones1 = document.getElementById("scroll1");
-    if (botones1.style.display != "none") {
-        botones1.style.display = "none"
-    } else {
+    console.log("eee", botones1.style.display)
+    if (botones1.style.display !== "flex") {
         botones1.style.display = "flex"
+    } else {
+        botones1.style.display = "none"
     }
 }
 // boton 2 hobbies
 function visibility2() {
     let botones2 = document.getElementById("scroll2");
-    if (botones2.style.display != "none") {
-        botones2.style.display = "none"
-    } else {
+    if (botones2.style.display !== "flex") {
         botones2.style.display = "flex"
+    } else {
+        botones2.style.display = "none"
     }
 }
 // boton 3 dibujo
 function visibility3() {
     let botones3 = document.getElementById("scroll3");
-    if (botones3.style.display != "none") {
-        botones3.style.display = "none"
-    } else {
+    if (botones3.style.display !== "flex") {
         botones3.style.display = "flex"
+    } else {
+        botones3.style.display = "none"
     }
 }
 // boton 4 sport
 function visibility4() {
     let botones4 = document.getElementById("scroll4");
 
-    if (botones4.style.display != "none") {
-        botones4.style.display = "none"
-    } else {
+    if (botones4.style.display !== "flex") {
         botones4.style.display = "flex"
+    } else {
+        botones4.style.display = "none"
     }
 }
 // mboton 5 anime
 function visibility5() {
     let botones5 = document.getElementById("scroll5");
 
-    if (botones5.style.display != "none") {
-        botones5.style.display = "none"
-    } else {
+    if (botones5.style.display !== "flex") {
         botones5.style.display = "flex"
+    } else {
+        botones5.style.display = "none"
     }
 }
 
@@ -50,10 +52,10 @@ function visibility5() {
 function visibility6() {
     let botones6 = document.getElementById("scroll6");
 
-    if (botones6.style.display != "none") {
-        botones6.style.display = "none"
-    } else {
+    if (botones6.style.display !== "flex") {
         botones6.style.display = "flex"
+    } else {
+        botones6.style.display = "none"
     }
 }
 
@@ -61,10 +63,10 @@ function visibility6() {
 function visibility7() {
     let botones7 = document.getElementById("scroll7");
 
-    if (botones7.style.display != "none") {
-        botones7.style.display = "none"
-    } else {
+    if (botones7.style.display !== "flex") {
         botones7.style.display = "flex"
+    } else {
+        botones7.style.display = "none"
     }
 }
 
@@ -72,9 +74,9 @@ function visibility7() {
 function visibility8() {
     let botones8 = document.getElementById("scroll8");
 
-    if (botones8.style.display != "none") {
-        botones8.style.display = "none"
-    } else {
+    if (botones8.style.display !== "flex") {
         botones8.style.display = "flex"
+    } else {
+        botones8.style.display = "none"
     }
 }
